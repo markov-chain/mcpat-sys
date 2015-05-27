04f767efeb78b79af26470b3ce117601c3b31ff8
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-05-26 20:40:26 -0400

    Compile McPAT as a shared library

---
+++ build.rs
@@ -0,0 +1,30 @@
+use std::{env, process};
+use std::path::PathBuf;
+
+macro_rules! cmd(
+    ($name:expr) => (process::Command::new($name));
+);
+
+macro_rules! get(
+    ($name:expr) => (env::var($name).unwrap_or(String::new()));
+);
+
+macro_rules! run(
+    ($command:expr) => (
+        assert!($command.stdout(process::Stdio::inherit())
+                        .stderr(process::Stdio::inherit())
+                        .status().unwrap().success());
+    );
+);
+
+fn main() {
+    let build = PathBuf::from(&get!("CARGO_MANIFEST_DIR")).join("build");
+    let output = PathBuf::from(&get!("OUT_DIR"));
+
+    run!(cmd!("make").arg("lib")
+                     .arg(&format!("-j{}", get!("NUM_JOBS")))
+                     .current_dir(&build));
+
+    println!("cargo:rustc-link-lib=dylib=mcpat");
+    println!("cargo:rustc-link-search={}", output.display());
+}
