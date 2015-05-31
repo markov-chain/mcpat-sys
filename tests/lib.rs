f6defa5313ba295e4a219686fc72a69aa62d6653
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-05-30 20:15:08 -0400

    Extend the test case

--- tests/lib.rs
+++ tests/lib.rs
@@ -42,22 +42,34 @@ unsafe fn display_energy(parsexml: *mut ParseXML, processor: *mut Processor) {
     let system = &*ParseXML_sys(parsexml);
 
     let long_channel = system.longer_channel_device != 0;
+    let power_gating = system.power_gating != 0;
 
     let power = Processor_power(processor);
     let readOp = powerDef_readOp(power);
 
-    let (dynamic, longer_channel_leakage, leakage, gate_leakage) = (
-        powerComponents_dynamic(readOp),
-        powerComponents_longer_channel_leakage(readOp),
-        powerComponents_leakage(readOp),
-        powerComponents_gate_leakage(readOp),
-    );
+    let dynamic = powerComponents_dynamic(readOp);
+    let leakage = powerComponents_leakage(readOp);
+    let gate_leakage = powerComponents_gate_leakage(readOp);
+    let longer_channel_leakage = powerComponents_longer_channel_leakage(readOp);
+    let power_gated_leakage = powerComponents_power_gated_leakage(readOp);
+    let power_gated_with_long_channel_leakage = powerComponents_power_gated_with_long_channel_leakage(readOp);
 
-    let peak_power = dynamic + if long_channel {
-                                   longer_channel_leakage
-                               } else {
-                                   leakage
-                               } + gate_leakage;
+    let peak_dynamic = dynamic;
+    let subthreshold_leakage = if long_channel { longer_channel_leakage } else { leakage };
+    let total_leakage = subthreshold_leakage + gate_leakage;
+    let peak_power = peak_dynamic + total_leakage;
+    let subthreshold_leakage_with_power_gating = if long_channel {
+                                                     power_gated_with_long_channel_leakage
+                                                 } else {
+                                                     power_gated_leakage
+                                                 };
 
     assert_eq!(round!(peak_power, 3), 134.938);
+    assert_eq!(round!(total_leakage, 4), 36.8319);
+    assert_eq!(round!(peak_dynamic, 4), 98.1063);
+    assert_eq!(round!(subthreshold_leakage, 4), 35.1632);
+    if power_gating {
+        assert_eq!(round!(subthreshold_leakage_with_power_gating, 4), 16.3977);
+    }
+    assert_eq!(round!(gate_leakage, 5), 1.66871);
 }
