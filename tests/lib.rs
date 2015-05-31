2e21e3b6b9100ca40a0f4cbca1b808618cddbcb0
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-05-31 07:28:47 -0400

    Refactor the test

--- tests/lib.rs
+++ tests/lib.rs
@@ -3,19 +3,21 @@
 extern crate mcpat_sys;
 
 use mcpat_sys::*;
+use std::collections::HashMap;
 
 macro_rules! ok(
     ($result:expr) => ($result.unwrap());
 );
 
-macro_rules! to_c_str(
+macro_rules! c_str(
     ($string:expr) => (ok!(::std::ffi::CString::new($string)));
 );
 
-macro_rules! round(
-    ($value:expr, $precision:expr) => ({
-        let scale = 10f64.powi($precision);
-        ($value * scale).round() / scale
+macro_rules! hash_map(
+    ($($key:expr => $value:expr,)*) => ({
+        let mut map = HashMap::new();
+        $(map.insert($key, $value);)*
+        map
     });
 );
 
@@ -24,7 +26,7 @@ macro_rules! round(
 // https://github.com/copies/mcpat/blob/master/ExampleResults/Xeon
 #[test]
 fn main() {
-    let filename = to_c_str!("build/source/ProcessorDescriptionFiles/Xeon.xml");
+    let filename = c_str!("build/source/ProcessorDescriptionFiles/Xeon.xml");
 
     unsafe {
         set_opt_for_clk(1);
@@ -43,11 +45,23 @@ fn main() {
 unsafe fn Processor_displayEnergy(processor: *mut Processor, parsexml: *mut ParseXML) {
     let system = &*ParseXML_sys(parsexml);
 
+    check(system, powerDef_readOp(Processor_power(processor)), hash_map!(
+        "Peak Power" => 134.938,
+        "Total Leakage" => 36.8319,
+        "Peak Dynamic" => 98.1063,
+        "Subthreshold Leakage" => 35.1632,
+        "Subthreshold Leakage with power gating" => 16.3977,
+        "Gate Leakage" => 1.66871,
+    ));
+
+    check(system, powerDef_readOp(Processor_rt_power(processor)), hash_map!(
+        "Runtime Dynamic" => 72.9199,
+    ));
+}
+
+unsafe fn check(system: &root_system, readOp: *mut powerComponents, map: HashMap<&str, f64>) {
     let long_channel = system.longer_channel_device != 0;
     let power_gating = system.power_gating != 0;
-
-    let power = Processor_power(processor);
-    let readOp = powerDef_readOp(power);
 
     let dynamic = powerComponents_dynamic(readOp);
     let leakage = powerComponents_leakage(readOp);
@@ -56,29 +70,54 @@ unsafe fn Processor_displayEnergy(processor: *mut Processor, parsexml: *mut Pars
     let power_gated_leakage = powerComponents_power_gated_leakage(readOp);
     let power_gated_with_long_channel_leakage = powerComponents_power_gated_with_long_channel_leakage(readOp);
 
-    let peak_dynamic = dynamic;
     let subthreshold_leakage = if long_channel { longer_channel_leakage } else { leakage };
     let total_leakage = subthreshold_leakage + gate_leakage;
-    let peak_power = peak_dynamic + total_leakage;
     let subthreshold_leakage_with_power_gating = if long_channel {
                                                      power_gated_with_long_channel_leakage
                                                  } else {
                                                      power_gated_leakage
                                                  };
 
-    assert_eq!(round!(peak_power, 3), 134.938);
-    assert_eq!(round!(total_leakage, 4), 36.8319);
-    assert_eq!(round!(peak_dynamic, 4), 98.1063);
-    assert_eq!(round!(subthreshold_leakage, 4), 35.1632);
-    if power_gating {
-        assert_eq!(round!(subthreshold_leakage_with_power_gating, 4), 16.3977);
+    for (key, value) in map {
+        match key {
+            "Peak Power" => {
+                equal(value, dynamic + total_leakage);
+            },
+            "Total Leakage" => {
+                equal(value, total_leakage);
+            },
+            "Peak Dynamic" => {
+                equal(value, dynamic);
+            },
+            "Subthreshold Leakage" => {
+                equal(value, subthreshold_leakage);
+            },
+            "Subthreshold Leakage with power gating" => {
+                assert!(power_gating);
+                equal(value, subthreshold_leakage_with_power_gating);
+            },
+            "Gate Leakage" => {
+                equal(value, gate_leakage);
+            },
+            "Runtime Dynamic" => {
+                equal(value, dynamic);
+            },
+            _ => {
+                assert!(false);
+            },
+        }
     }
-    assert_eq!(round!(gate_leakage, 5), 1.66871);
+}
 
-    let power_rt = Processor_rt_power(processor);
-    let readOp = powerDef_readOp(power_rt);
-
-    let runtime_dynamic = powerComponents_dynamic(readOp);
-
-    assert_eq!(round!(runtime_dynamic, 4), 72.9199);
+fn equal(expected: f64, actual: f64) {
+    let (mut precision, mut scale) = (1i32, 1f64);
+    loop {
+        if (expected * scale).round() / scale == expected {
+            break;
+        }
+        precision += 1;
+        scale = 10f64.powi(precision);
+        assert!(precision < 16);
+    }
+    assert_eq!((actual * scale).round() / scale, expected);
 }
