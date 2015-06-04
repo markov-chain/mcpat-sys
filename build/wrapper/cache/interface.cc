a0322a220922a19ec76598a3d78ec5a0c377d611
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-04 11:38:56 -0400

    Make caching thread safe

--- build/wrapper/cache/interface.cc
+++ build/wrapper/cache/interface.cc
@@ -1,20 +1,35 @@
+#include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include <hiredis/hiredis.h>
 #include <wrapper/cache/interface.h>
 
-static bool cache_initialized = false;
-static char cache_host[1024];
-static int cache_port;
+struct {
+	pthread_mutex_t mutex;
+	bool initialized;
+	bool available;
+	char host[1024];
+	int port;
+} cache_state = {
+	.mutex = PTHREAD_MUTEX_INITIALIZER,
+	.initialized = false,
+	.available = false,
+	.host = {0},
+	.port = 0
+};
 
 static void cache_initialize();
 
 bool cache_get(InputParameter *, uca_org_t *, void **) {
-	if (!cache_initialized) {
-		cache_initialize();
-		cache_initialized = true;
+	if (!cache_state.initialized) {
+		pthread_mutex_lock(&cache_state.mutex);
+		if (!cache_state.initialized) cache_initialize();
+		pthread_mutex_unlock(&cache_state.mutex);
 	}
+
+	if (!cache_state.available) return false;
+
 	return false;
 }
 
@@ -23,21 +38,20 @@ void cache_set(InputParameter *, uca_org_t *, void **) {
 
 static void cache_initialize() {
 	char *result;
-	
-	memset((void *)cache_host, 0, sizeof(cache_host));
-	cache_port = 0;
 
 	result = getenv(CACHE_HOST_ENV);
 	if (result && strlen(result) > 0) {
-		strncpy(cache_host, result, sizeof(cache_host) - 1);
+		strncpy(cache_state.host, result, sizeof(cache_state.host) - 1);
 	} else {
-		strncpy(cache_host, CACHE_HOST_DEF, sizeof(cache_host) - 1);
+		strncpy(cache_state.host, CACHE_HOST_DEF, sizeof(cache_state.host) - 1);
 	}
 
 	result = getenv(CACHE_PORT_ENV);
 	if (result && strlen(result) > 0) {
-		cache_port = atoi(result);
+		cache_state.port = atoi(result);
 	} else {
-		cache_port = CACHE_PORT_DEF;
+		cache_state.port = CACHE_PORT_DEF;
 	}
+
+	cache_state.initialized = true;
 }
