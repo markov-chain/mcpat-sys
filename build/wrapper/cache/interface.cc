fc49c2ed60548601c738c93d3e9fc4e708470101
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-04 10:45:46 -0400

    Link to Hiredis

--- build/wrapper/cache/interface.cc
+++ build/wrapper/cache/interface.cc
@@ -1,8 +1,43 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include <hiredis/hiredis.h>
 #include <wrapper/cache/interface.h>
 
+static bool cache_initialized = false;
+static char cache_host[1024];
+static int cache_port;
+
+static void cache_initialize();
+
 bool cache_get(InputParameter *, uca_org_t *, void **) {
+	if (!cache_initialized) {
+		cache_initialize();
+		cache_initialized = true;
+	}
 	return false;
 }
 
 void cache_set(InputParameter *, uca_org_t *, void **) {
+}
+
+static void cache_initialize() {
+	char *result;
+	
+	memset((void *)cache_host, 0, sizeof(cache_host));
+	cache_port = 0;
+
+	result = getenv(CACHE_HOST_ENV);
+	if (result && strlen(result) > 0) {
+		strncpy(cache_host, result, sizeof(cache_host) - 1);
+	} else {
+		strncpy(cache_host, CACHE_HOST_DEF, sizeof(cache_host) - 1);
+	}
+
+	result = getenv(CACHE_PORT_ENV);
+	if (result && strlen(result) > 0) {
+		cache_port = atoi(result);
+	} else {
+		cache_port = CACHE_PORT_DEF;
+	}
 }
--- build/wrapper/cache/interface.h
+++ build/wrapper/cache/interface.h
@@ -1,7 +1,14 @@
-#ifndef __CACHE_INTERFACE_H__
-#define __CACHE_INTERFACE_H__
+#ifndef ___CACHE_INTERFACE_H___
+#define ___CACHE_INTERFACE_H___
 
+#include <source/cacti/Ucache.h>
 #include <source/cacti/cacti_interface.h>
+
+#define CACHE_HOST_ENV "MCPAT_CACHE_HOST"
+#define CACHE_PORT_ENV "MCPAT_CACHE_PORT"
+
+#define CACHE_HOST_DEF "127.0.0.1"
+#define CACHE_PORT_DEF 6379
 
 bool cache_get(InputParameter *, uca_org_t *, void **);
 void cache_set(InputParameter *, uca_org_t *, void **);
