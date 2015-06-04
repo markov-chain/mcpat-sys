6676f551c8899a3a97bb0d378ecbd8514dcc1ee5
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-04 13:14:22 -0400

    Connect to Redis

--- build/wrapper/cache/interface.cc
+++ build/wrapper/cache/interface.cc
@@ -8,15 +8,15 @@
 struct {
 	pthread_mutex_t mutex;
 	bool initialized;
-	bool available;
 	char host[1024];
 	int port;
+	redisContext *redis;
 } cache_state = {
 	.mutex = PTHREAD_MUTEX_INITIALIZER,
 	.initialized = false,
-	.available = false,
 	.host = {0},
-	.port = 0
+	.port = 0,
+	.redis = NULL
 };
 
 static void cache_initialize();
@@ -24,11 +24,14 @@ static void cache_initialize();
 bool cache_get(InputParameter *, uca_org_t *, void **) {
 	if (!cache_state.initialized) {
 		pthread_mutex_lock(&cache_state.mutex);
-		if (!cache_state.initialized) cache_initialize();
+		if (!cache_state.initialized) {
+			cache_initialize();
+			cache_state.initialized = true;
+		}
 		pthread_mutex_unlock(&cache_state.mutex);
 	}
 
-	if (!cache_state.available) return false;
+	if (!cache_state.redis) return false;
 
 	return false;
 }
@@ -53,5 +56,18 @@ static void cache_initialize() {
 		cache_state.port = CACHE_PORT_DEF;
 	}
 
-	cache_state.initialized = true;
+	redisContext *redis = redisConnect(cache_state.host, cache_state.port);
+	if (!redis) {
+		printf("Cache: failed to initialize.\n");
+		return;
+	}
+
+	if (redis->err) {
+		printf("Cache: failed to connect to %s:%d (%s).\n",
+			cache_state.host, cache_state.port, redis->errstr);
+		redisFree(redis);
+		return;
+	}
+
+	cache_state.redis = redis;
 }
