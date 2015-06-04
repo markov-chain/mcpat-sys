d77451c5924981a16e61366bc2806d7502ca5d90
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-04 19:40:16 -0400

    Refine caching

--- build/wrapper/cache/interface.cc
+++ build/wrapper/cache/interface.cc
@@ -1,46 +1,115 @@
-#include <pthread.h>
+// The code below is not thread safe. There is no point in pursuing thread
+// safeness here as McPAT internally is not thread safe anyway.
+
 #include <stdlib.h>
 #include <string.h>
 
 #include <hiredis/hiredis.h>
 #include <wrapper/cache/interface.h>
 
+#define CACHE_HOST_ENV "MCPAT_CACHE_HOST"
+#define CACHE_PORT_ENV "MCPAT_CACHE_PORT"
+
+#define CACHE_HOST_DEF "127.0.0.1"
+#define CACHE_PORT_DEF 6379
+
+#define ZERO_FIELD(field) (memset((void *)&(field), 0, sizeof(field)))
+
 struct {
-	pthread_mutex_t mutex;
 	bool initialized;
+	bool available;
 	char host[1024];
 	int port;
 	redisContext *redis;
 } cache_state = {
-	.mutex = PTHREAD_MUTEX_INITIALIZER,
 	.initialized = false,
-	.host = {0},
-	.port = 0,
-	.redis = NULL
+	.available = false
 };
 
-static void cache_initialize();
+static void cache_initialize(void);
+static void cache_deinitialize(void);
+static void cache_serialize(cache_value_t *, uca_org_t *);
+static void cache_deserialize(cache_value_t *, uca_org_t *);
 
-bool cache_get(InputParameter *, uca_org_t *, void **) {
+void cache_key(cache_key_t *key, InputParameter *input) {
 	if (!cache_state.initialized) {
-		pthread_mutex_lock(&cache_state.mutex);
-		if (!cache_state.initialized) {
-			cache_initialize();
-			cache_state.initialized = true;
-		}
-		pthread_mutex_unlock(&cache_state.mutex);
+		cache_initialize();
+		cache_state.initialized = true;
 	}
 
-	if (!cache_state.redis) return false;
+	if (!cache_state.available) return;
 
-	return false;
+	key->base = *input;
+	ZERO_FIELD(key->base.dvs_voltage);
 }
 
-void cache_set(InputParameter *, uca_org_t *, void **) {
+bool cache_get(cache_key_t *key, uca_org_t *output) {
+	if (!cache_state.available) return false;
+
+	const int argc = 2;
+	const char *argv[argc] = {"GET", (char *)key};
+	size_t argvlen[argc] = {3, sizeof(cache_key_t)};
+
+	redisReply *reply = (redisReply *)redisCommandArgv(
+		cache_state.redis, argc, argv, argvlen);
+
+	if (!reply) {
+		printf("Cache: failed to get (%s).\n", cache_state.redis->errstr);
+		cache_deinitialize();
+		return false;
+	}
+
+	if (reply->type == REDIS_REPLY_ERROR) {
+		printf("Cache: failed to get (%s).\n", reply->str);
+		freeReplyObject((void *)reply);
+		cache_deinitialize();
+		return false;
+	}
+
+	if (reply->len != sizeof(cache_value_t)) {
+		printf("Cache: received corrupted data.\n");
+		freeReplyObject((void *)reply);
+		cache_deinitialize();
+		return false;
+	}
+
+	cache_deserialize((cache_value_t *)reply->str, output);
+	freeReplyObject((void *)reply);
+
+	return true;
+}
+
+void cache_set(cache_key_t *key, uca_org_t *output) {
+	if (!cache_state.available) return;
+
+	cache_value_t value;
+	cache_serialize(&value, output);
+
+	const int argc = 3;
+	const char *argv[argc] = {"SET", (char *)key, (char *)&value};
+	size_t argvlen[argc] = {3, sizeof(cache_key_t), sizeof(cache_value_t)};
+
+	redisReply *reply = (redisReply *)redisCommandArgv(
+		cache_state.redis, argc, argv, argvlen);
+
+	if (!reply) {
+		printf("Cache: failed to set (%s).\n", cache_state.redis->errstr);
+		cache_deinitialize();
+		return;
+	}
+
+	if (reply->type == REDIS_REPLY_ERROR) {
+		printf("Cache: failed to set (%s).\n", reply->str);
+		cache_deinitialize();
+	}
+
+	freeReplyObject((void *)reply);
 }
 
 static void cache_initialize() {
 	char *result;
+
+	ZERO_FIELD(cache_state);
 
 	result = getenv(CACHE_HOST_ENV);
 	if (result && strlen(result) > 0) {
@@ -70,4 +139,48 @@ static void cache_initialize() {
 	}
 
 	cache_state.redis = redis;
+	cache_state.available = true;
+}
+
+static void cache_deinitialize() {
+	if (cache_state.redis) {
+		redisFree(cache_state.redis);
+	}
+	ZERO_FIELD(cache_state);
+}
+
+static void cache_serialize(cache_value_t *value, uca_org_t *output) {
+	ZERO_FIELD(*value);
+
+	value->base = *output;
+	value->base.tag_array2 = NULL;
+	value->base.data_array2 = NULL;
+	ZERO_FIELD(value->base.uca_q);
+	value->base.uca_pg_reference = NULL;
+
+	if (output->tag_array2) {
+		value->tag_array2 = *output->tag_array2;
+		value->tag_array2.arr_min = NULL;
+	}
+
+	if (output->data_array2) {
+		value->data_array2 = *output->data_array2;
+		value->data_array2.arr_min = NULL;
+	}
+}
+
+static void cache_deserialize(cache_value_t *value, uca_org_t *output) {
+	mem_array *tag_array2 = output->tag_array2;
+	if (tag_array2) {
+		*tag_array2 = value->tag_array2;
+	}
+
+	mem_array *data_array2 = output->data_array2;
+	if (data_array2) {
+		*data_array2 = value->data_array2;
+	}
+
+	*output = value->base;
+	output->tag_array2 = tag_array2;
+	output->data_array2 = data_array2;
 }
--- build/wrapper/cache/interface.h
+++ build/wrapper/cache/interface.h
@@ -4,13 +4,21 @@
 #include <source/cacti/Ucache.h>
 #include <source/cacti/cacti_interface.h>
 
-#define CACHE_HOST_ENV "MCPAT_CACHE_HOST"
-#define CACHE_PORT_ENV "MCPAT_CACHE_PORT"
+typedef struct __InputParameter {
+	InputParameter base;
+} __InputParameter;
 
-#define CACHE_HOST_DEF "127.0.0.1"
-#define CACHE_PORT_DEF 6379
+typedef struct __uca_org_t {
+	uca_org_t base;
+	mem_array tag_array2;
+	mem_array data_array2;
+} __uca_org_t;
 
-bool cache_get(InputParameter *, uca_org_t *, void **);
-void cache_set(InputParameter *, uca_org_t *, void **);
+typedef __InputParameter cache_key_t;
+typedef __uca_org_t cache_value_t;
+
+void cache_key(cache_key_t *, InputParameter *);
+bool cache_get(cache_key_t *, uca_org_t *);
+void cache_set(cache_key_t *, uca_org_t *);
 
 #endif
